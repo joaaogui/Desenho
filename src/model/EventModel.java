529ba4f1fa37044b3d6f1f413b9d3205682ec66e
João Guilherme <joaaogui@gmail.com>
2016-05-22 23:27:24 -0300

    Fixing get and set

--- src/model/EventModel.java
+++ src/model/EventModel.java
@@ -1,5 +1,37 @@
 package model;
 
 public class EventModel {
+	
+	private String dataInicial;
+	private String dataFinal;
+	private String observação;
+	private String tipoEvento;
+	
+	public String getDataInicial() {
+		return dataInicial;
+	}
+	public void setDataInicial(String dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+	public String getObservação() {
+		return observação;
+	}
+	public void setObservação(String observação) {
+		this.observação = observação;
+	}
+	public String getTipoEvento() {
+		return tipoEvento;
+	}
+	public void setTipoEvento(String tipoEvento) {
+		this.tipoEvento = tipoEvento;
+	}
+	public String getDataFinal() {
+		return dataFinal;
+	}
+	public void setDataFinal(String dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	
+	
 
 }
